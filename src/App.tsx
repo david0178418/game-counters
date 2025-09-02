3873ba0075ab88f40f94d85b15d2feff34b0cfe9
David Granado <davidjgranado@gmail.com>
2025-09-01 22:54:03 -0500

    Adjust fullscreen button to a fab button

--- src/App.tsx
+++ src/App.tsx
@@ -359,17 +359,6 @@ export function App() {
         </div>
         <div className="header-buttons">
           <ThemeToggle theme={theme} onToggle={toggleTheme} />
-          {counters.length > 0 && (
-            <button
-              type="button"
-              className="maximize-button"
-              onClick={toggleMaximized}
-              aria-label={isMaximized ? "Exit fullscreen" : "Enter fullscreen"}
-              title={isMaximized ? "Exit fullscreen" : "Enter fullscreen"}
-            >
-              {isMaximized ? "⮟" : "⛶"}
-            </button>
-          )}
           <button
             type="button"
             className="add-counter-button"
@@ -500,6 +489,27 @@ export function App() {
           />
         ))}
       </div>
+
+      {!isMaximized && counters.length > 0 && (
+        <button
+          type="button"
+          className="floating-maximize-button"
+          onClick={toggleMaximized}
+          aria-label="Enter fullscreen"
+          title="Enter fullscreen"
+        >
+          <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            width="20" 
+            height="20" 
+            fill="currentColor" 
+            viewBox="0 0 16 16"
+            className="fullscreen-enter-icon"
+          >
+            <path d="M1.5 1a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4A1.5 1.5 0 0 1 1.5 0h4a.5.5 0 0 1 0 1zM10 .5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 16 1.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5M.5 10a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 0 14.5v-4a.5.5 0 0 1 .5-.5m15 0a.5.5 0 0 1 .5.5v4a1.5 1.5 0 0 1-1.5 1.5h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5"/>
+          </svg>
+        </button>
+      )}
 
       {!isMaximized && showCollectionManager && (
         <CollectionManager
